238920784e46f1cd334bc103539e6357da962a99
mendezc0251 <cmendez0251@gmail.com>
2025-06-14 21:44:39 -0500

    fixed canvas dpr and element positions

--- src/game/initCanvas.js
+++ src/game/initCanvas.js
@@ -1,10 +1,29 @@
 import headerBacking from '../assets/HeaderA.png'
 
 export function initCanvas(canvas){
-    
-    
-    const container = canvas.parentElement
     const ctx = canvas.getContext('2d')
+    ctx.imageSmoothingEnabled = false
+    canvas.style.imageRendering = 'pixelated'
+    const dpr = window.devicePixelRatio || 1
+
+    const displayWidth = 1280
+    const displayHeight = 720
+
+    canvas.width = 1280*dpr
+    canvas.height = 720*dpr
+
+    canvas.style.width = displayWidth + "px"
+    canvas.style.height = displayHeight + "px"
+
+    ctx.scale(dpr,dpr)
+
+    canvas.addEventListener("click", function (event){
+        const rect = canvas.getBoundingClientRect()
+        const mouseX = event.clientX - rect.left
+        const mouseY = event.clientY - rect.top
+
+        handleClick(mouseX, mouseY)
+    })
 
     let currentScene = "menu";
 
@@ -33,54 +52,48 @@ export function initCanvas(canvas){
             renderChar();
         }
     }
-
+    //Instantiate header image
     const headerBack = new Image()
     headerBack.src = headerBacking
 
-    function renderMenu(){
-        const buttonWidth = canvas.width*0.2 //20% width button
-        const buttonHeight = canvas.height*0.1 //10% height button
+    //Set button width and location on canvas
+    const buttonWidth = displayWidth*0.3 //20% width button
+    const buttonHeight = displayHeight*0.15 //10% height button
 
-        const buttonX = (canvas.width - buttonWidth) / 2 // center horizontal
-        const buttonY = canvas.height*0.5 //50% from top
+    const buttonX = (displayWidth - buttonWidth) / 2 // center horizontal
+    const buttonY = displayHeight*0.6 //50% from top
+
+    const buttons = [
+        {text: "Play", x:buttonX, y:buttonY, width:buttonWidth, height:buttonHeight, onClick: () => currentScene="characterSelect"}
+    ]
+    function renderMenu(){
         
-        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        
+        ctx.clearRect(0, 0, displayWidth, displayHeight)
 
         ctx.fillStyle = "#2D2D2D"
-        ctx.fillRect(0,0,canvas.width, canvas.height)
+        ctx.fillRect(0,0,displayWidth, displayHeight)
 
-        ctx.fillStyle = "#FFD97D"
-        ctx.fillRect(buttonX, buttonY, buttonWidth, buttonHeight)
+        const scale = 1
 
-        const canvasAspect = canvas.width/canvas.height
-        const headerAspect = headerBack.width/headerBack.height
-
-        let scale
-        if (canvasAspect>headerAspect){
-            scale = canvas.height/headerBack.height
-        } else {
-            scale = canvas.width/headerBack.width
-        }
-        scale = Math.floor(scale)
-
-        const newWidth = headerBack.width*scale
-        const newHeight = headerBack.height*scale
-
-        if (headerBack.complete){
-            ctx.imageSmoothingEnabled = false
-            
-            ctx.drawImage(headerBack, (canvas.width-newWidth)/2,(canvas.height-newHeight)*0.4,newWidth,newHeight)
+        if(headerBack.complete){
+            ctx.drawImage(headerBack, (displayWidth-headerBack.width*scale)*0.5, (displayHeight-headerBack.height*scale)*0.15, headerBack.width*scale,headerBack.height*scale)
         }
 
-        document.fonts.ready.then(()=>{
-            ctx.font = `${buttonWidth*0.10}px "Press Start 2P"`
+        buttons.forEach(button =>{
+            ctx.fillStyle = "#FFD97D"
+            ctx.fillRect(button.x, button.y, button.width, button.height)
+            ctx.font = '40px "Press Start 2P"'
             ctx.fillStyle = "#F9F7F1"
             ctx.textAlign = "center"
             ctx.textBaseline = "middle"
-            ctx.fillText("Play!", buttonX+(buttonWidth/2), buttonY+(buttonHeight/2))
-            
+            ctx.fillText(button.text, button.x+(button.width/2), button.y+(button.height/2))
+
+        })
+
+        document.fonts.ready.then(()=>{
             ctx.fillStyle = "#F9F7F1"
-            ctx.fillText("Roguelite Jamboree", (canvas.width)*0.5, (canvas.height)*0.4)
+            ctx.fillText("Roguelite Jamboree",(displayWidth)*0.5,(displayHeight)*0.3)
 
         })
         
