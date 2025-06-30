5c7bce59d01de76407f0fed7d0ba1c9703f9dd97
mendezc0251 <cmendez0251@gmail.com>
2025-06-30 16:46:16 -0500

    test fix for image paths on deployed site

--- frontend/src/game/Resources.js
+++ frontend/src/game/Resources.js
@@ -3,14 +3,14 @@ class Resources{
     constructor(){
         // Everything we want to download
         this.toLoad = {
-            headerBack: '../../src/assets/HeaderA.png',
-            peg: '../../src/assets/Peg.png',
-            hitPeg: '../../src/assets/PegHit.png',
-            coin: '../../src/assets/Coin.png',
-            pBackImg1: '../../src/assets/cloud1.png',
-            pBackImg2: '../../src/assets/cloud2.png',
-            pBackImg3: '../../src/assets/cloud3.png',
-            pBackImg4: '../../src/assets/cloud4.png',
+            headerBack: '/assets/HeaderA.png',
+            peg: '/assets/Peg.png',
+            hitPeg: '/assets/PegHit.png',
+            coin: '/assets/Coin.png',
+            pBackImg1: '/assets/cloud1.png',
+            pBackImg2: '/assets/cloud2.png',
+            pBackImg3: '/assets/cloud3.png',
+            pBackImg4: '/assets/cloud4.png',
 
         };
 
