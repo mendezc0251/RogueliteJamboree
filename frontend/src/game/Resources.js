ea6dc28bdfcbfdfb58aaf08fd439151d262f655e
mendezc0251 <cmendez0251@gmail.com>
2025-06-30 16:40:30 -0500

    fixed paths for images

--- frontend/src/game/Resources.js
+++ frontend/src/game/Resources.js
@@ -3,14 +3,14 @@ class Resources{
     constructor(){
         // Everything we want to download
         this.toLoad = {
-            headerBack: '../../public/assets/HeaderA.png',
-            peg: '../../public/assets/Peg.png',
-            hitPeg: '../../public/assets/PegHit.png',
-            coin: '../../public/assets/Coin.png',
-            pBackImg1: '../../public/assets/cloud1.png',
-            pBackImg2: '../../public/assets/cloud2.png',
-            pBackImg3: '../../public/assets/cloud3.png',
-            pBackImg4: '../../public/assets/cloud4.png',
+            headerBack: '../../src/assets/HeaderA.png',
+            peg: '../../src/assets/Peg.png',
+            hitPeg: '../../src/assets/PegHit.png',
+            coin: '../../src/assets/Coin.png',
+            pBackImg1: '../../src/assets/cloud1.png',
+            pBackImg2: '../../src/assets/cloud2.png',
+            pBackImg3: '../../src/assets/cloud3.png',
+            pBackImg4: '../../src/assets/cloud4.png',
 
         };
 
