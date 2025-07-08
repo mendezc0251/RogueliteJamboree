4939e5ed9447a2e853c64e001bb4efd8c8399354
mendezc0251 <cmendez0251@gmail.com>
2025-07-08 00:12:15 -0500

    Added modal to prompt user sesssion

--- frontend/src/Game.jsx
+++ frontend/src/Game.jsx
@@ -1,20 +1,40 @@
 import { useRef, useState, useEffect } from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import {initCanvas} from './game/initCanvas'
 import './App.css'
+import IntroModal from './IntroModal'
+
 function Game(){
     const canvasRef = useRef(null)
     const location = useLocation()
+    const navigate = useNavigate()
   
     useEffect(() =>{
         if (location.pathname==='/' && canvasRef.current) {
         initCanvas(canvasRef.current)
         }
     }, [location.pathname]);
+
+    const handleGuest = () =>{
+        console.log("parent handleGuest called")
+        if(!localStorage.getItem("rj_gues_data")){
+            localStorage.setItem("rj_guest_data", JSON.stringify({
+                pachinkoHighscore:0,
+                pachinkoUpgrades: []
+            }));
+        }
+    }
+
+    const handleLogin = () =>{
+        navigate('/login');
+    }
+
     return(
         <div className='game-container'>
             <canvas className="game" ref={canvasRef}></canvas>
+            <IntroModal onPlayGuest={handleGuest} onLogin={handleLogin}/>
         </div>
+        
     )
 }
 export default Game
---
+++ frontend/src/Login.jsx
@@ -0,0 +1,4 @@
+function Login(){
+    return <h1>This page is a work in progress...</h1>
+}
+export default Login
