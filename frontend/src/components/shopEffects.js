b3f07d5b48d0f0aea6ab1a689577a576da1d4f35
mendezc0251 <cmendez0251@gmail.com>
2025-07-15 17:06:49 -0500

    Added more shop upgrades and highscore and point tracking

--- frontend/src/components/shopEffects.js
+++ frontend/src/components/shopEffects.js
@@ -1,9 +1,25 @@
+// class to export shopEffects that change pachinkoGameState on purchase
+
 export const shopEffects ={
     addCoin: (state) => {
         console.log("extra coin added!")
         state.coins = state.coins + 1 
     },
+    addTwoCoin: (state) => {
+        console.log("extra coins added!")
+        state.coins = state.coins + 2 
+    },
+    addThreeCoin: (state) => {
+        console.log("extra coins added!")
+        state.coins = state.coins + 3 
+    },
     addBounce: (state) => {
         state.bfNum = state.bfNum + .1
+    },
+    addTwoBounce: (state) => {
+        state.bfNum = state.bfNum + .2
+    },
+    addThreeBounce: (state) => {
+        state.bfNum = state.bfNum + .3
     }
 }
