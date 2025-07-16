693c54786002e5f50575f1e6977c7193bb221ca4
mendezc0251 <cmendez0251@gmail.com>
2025-07-16 16:28:05 -0500

    Added skeleton for login form and skeleton for login backend post

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,4 +1,27 @@
-function Login(){
-    return <h1>This page is a work in progress...</h1>
+import './Login.css'
+
+function Login() {
+    return (
+        <>
+            <div className="login-container">
+                <div className="login-form">
+                    <form action="">
+                        <h1>Login</h1>
+                        <input className="user" type="text" name="username" placeholder="Username" required></input>
+                        <input className="pass" type="text" name="password" placeholder="Password" required></input>
+                        <div className="remember-forgot">
+                            <label>
+                                <input type="checkbox" name="remember"/>
+                                Remember Me
+                            </label>
+                            <a>Forgot Password?</a>
+                        </div>
+                        <button type="submit">Log In</button>
+                        <p>Don't have an account? <a>Register</a></p>
+                    </form>
+                </div>
+            </div>
+        </>
+    )
 }
 export default Login
