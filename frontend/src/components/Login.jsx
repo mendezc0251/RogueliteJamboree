64c3c38ee45750c193ee310a3b589740be3056db
mendezc0251 <cmendez0251@gmail.com>
2025-07-20 21:56:08 -0500

    added password strength check to register form

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -8,6 +8,8 @@ function Login() {
     const [message, setMessage] = useState('')
     const [email, setEmail] = useState('')
     const [authMode, setAuthMode] = useState('login')
+    const [feedback, setFeedback] = useState('')
+    const [strongPassword, setStrongPassword] = useState(false)
 
     const toggleAuthMode = () => {
         setAuthMode(authMode === 'login' ? 'register' : 'login')
@@ -61,6 +63,29 @@ function Login() {
         }
     }
 
+    const handleChange= (e) =>{
+        const newPwd = e.target.value
+        setPassword(newPwd)
+        validatePassword(newPwd)
+    }
+
+    const validatePassword = (pwd) => {
+        const errors = []
+        if(pwd.length<8) errors.push("at least 8 characters")
+        if(!/[a-z]/.test(pwd)) errors.push("a lowercase letter")
+        if(!/[A-Z]/.test(pwd)) errors.push("a uppercase letter")
+        if(!/[1-9]/.test(pwd)) errors.push("a number")
+        if(!/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(pwd)) errors.push("a symbol")
+
+        if(errors.length===0){
+            setFeedback("Strong password!")
+            setStrongPassword(false)
+        } else {
+            setFeedback("Needs: " + errors.join(", "))
+            setStrongPassword(true)
+        }
+    }
+
     return (
         <>
             <div className="login-container">
@@ -69,7 +94,7 @@ function Login() {
                         <form onSubmit={handleLogin} action="">
                             <h1>Login</h1>
                             <input className="user" type="text" name="username" placeholder="username" value={username} onChange={(e) => setUsername(e.target.value)} required></input>
-                            <input className="pass" type="text" name="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} required></input>
+                            <input className="pass" type="password" name="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} required></input>
                             <div className="remember-forgot">
                                 <label>
                                     <input type="checkbox" name="remember" />
@@ -86,7 +111,8 @@ function Login() {
                             <h1>Register</h1>
                             <input className="email" type="text" name="email" placeholder="email@email.com" value={email} onChange={(e) => setEmail(e.target.value)} required></input>
                             <input className="user" type="text" name="username" placeholder="username" value={username} onChange={(e) => setUsername(e.target.value)} required></input>
-                            <input className="pass" type="text" name="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} required></input>
+                            <input className="pass" type="password" name="password" placeholder="password" value={password} onChange={handleChange} required></input>
+                            <p className={strongPassword?"feedback":"good-feedback"}>{feedback}</p>
                             <div className="remember-forgot">
                                 <label>
                                     <input type="checkbox" name="remember" />
@@ -95,7 +121,7 @@ function Login() {
                                 <a>Forgot Password?</a>
                             </div>
                             {message && <p>{message}</p>}
-                            <button type="submit">Register</button>
+                            <button type="submit" disabled={strongPassword}>Register</button>
                             <p>Already have an account? <a onClick={toggleAuthMode}>Login</a></p>
                         </form>
                     )}
